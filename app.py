6aa80f72bb7465fb8fda6d923c4cc838a4c92749
sunfireaegis <henry_morgan06@mail.ru>
2022-12-19 00:10:13 +0300

    added logging

--- app.py
+++ app.py
@@ -5,78 +5,92 @@ from werkzeug.utils import secure_filename
 from datetime import datetime
 import os
 import csv
+import logging
+
+
+if not os.path.exists('log'):
+    os.system('touch log')
+
+flaskLogger = logging.getLogger('werkzeug')
+flaskLogger.setLevel(logging.CRITICAL) # suppressing some unnecesary logs
+logging.basicConfig(filename='log', level=logging.DEBUG)
 
 connection = sqlite3.connect("database.db", check_same_thread=False)
 cursor = connection.cursor()
 
 app = Flask("voice_recorder")
 
+if not os.path.exists('files/'):
+    os.mkdir('files')
+
 app.config['UPLOAD_FOLDER'] = 'files/'
 
 folder = "/home/main/projects/voice-recording-web"
 
-to_read = list()
-with open("task.csv", "r") as task:
+to_read = dict()
+with open("csv/task.csv", "r") as task:
     reader = csv.reader(task, delimiter=',')
     head = reader.__next__()
     for i, elem in enumerate(reader):
-        to_read.append([elem, False, None, set()])  # data{text, task}, already_read, taken, skipped_by{uname}
+        to_read.update({elem[2]: [elem[:2], False, None, set()]})  # tasl_id: data{text, task}, already_read, taken, skipped_by{uname}
 
 
 
-@app.route("/index/<uname>")
-def page_after_auth(uname):
-    
+@app.route("/index", methods=['POST'])
+def page_after_auth():
+    uname = request.args['uname']
     # app.logger.warning(to_read)
-    for i, el in enumerate(to_read):
-        
-        print((el[1], el[2], str(uname)))
-        if not el[1] and not (uname in el[3]) and el[2] is None:
-            el[2] = uname
-            print(el[2], el[3])
-            return render_template("index.html", title="testt", text=el[0][0], task=el[0][1], n=i)
-        elif el[2] == uname:
-            print(1)
-            return render_template("index.html", title="testt", text=el[0][0], task=el[0][1], n=i)
+    for el in to_read:
+        cur = to_read[el]
+        if not cur[1] and not (uname in cur[3]) and cur[2] is None: # if text is free yet
+            to_read[el][2] = uname
+            return render_template("index.html", title="testt", text=cur[0][0], task=cur[0][1], n=el)
+        elif cur[2] == uname: # if you reloaded page (state of 'taken' arg is the same)
+            return render_template("index.html", title="testt", text=cur[0][0], task=cur[0][1], n=el)
 
+    print(os.listdir('files/'))
 
     return render_template('index.html', title='task', text="Заданий больше нет", task=None, n=-1)
 
 
 @app.route("/", methods=["GET"])
 def main():
-    return redirect(url_for("auth"))  # redirect(...)
+    return redirect(url_for("auth"))  # redirect to auth page
 
 
 @app.route("/recording", methods=["POST"])
 def get_file():
     if request.method == "POST":
-        app.logger.warning('log begin')
-        # app.logger.warning(dir(request), request.form.get('author'))
         file = request.files.get('voice')
         author = request.form.get('author')
         cur_task = request.form.get('cur_task')
         is_skipped = request.form.get('skip')
 
-        # app.logger.warning(type(is_skipped))
+        try:
+            to_read[cur_task][2] = False # current text broke free
+        except KeyError:
+            app.logger.warning('Unable to save EOF')
+
+
 
         if is_skipped =='true': # handling text skipping
-            app.logger.warning('Task Skipped')
-            to_read[int(cur_task)][2] = False # current text broke free
-            to_read[int(cur_task)][3].add(author)
+            logging.warning(f'Task {cur_task} Skipped')
+            try:
+                to_read[cur_task][3].add(author)
+            except KeyError:
+                app.logger.warning('Unable to save EOF')
         elif cur_task != '-1': # else saving file
-            to_read[int(cur_task)][2] = False # current text broke free
+            try:
+                to_read[cur_task][1] = True # setting that handled text is already read
+            except KeyError:
+                app.logger.warning('Unable to save EOF')
 
-            filename = secure_filename(file.filename) + f'{datetime.now()}_{author}_{cur_task}' + '.wav' 
-            to_read[int(cur_task)][1] = True # setting that handled text is already read
+            filename = f'id_{cur_task}_{author}_{datetime.now()}' + '.wav' 
             
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            logging.info(f'file {filename} is written correctly')
 
-
-        # with connection:
-        #     cursor.execute("UPDATE users SET ")
-        # app.logger.warning(to_read)
-        return redirect(url_for("page_after_auth", uname=author))
+        return redirect(url_for("page_after_auth", uname=author), code=307)
 
 
 @app.route("/reg", methods=["POST", "GET"])
@@ -88,7 +102,7 @@ def reg():
         with connection:
             successful_auth = cursor.execute("INSERT INTO users (login, password) VALUES (?, ?)", (login, password,))
             print(successful_auth)
-
+        logging.info(f'user {login} registrated succesfully')
         return redirect(url_for("page_after_auth", uname=login))
     return render_template("reg.html")
 
@@ -105,12 +119,15 @@ def auth():
             app.logger.warning(successful_auth)
         if successful_auth:
             # return redirect(url_for("page_after_auth"), )\
-            response = make_response(redirect(url_for("page_after_auth", uname=login)))
+            response = make_response(redirect(url_for("page_after_auth", uname=login), code=307))
             response.set_cookie('uname', login)
+            logging.info(f'user {login} authorized succesfully')
             return response
+        logging.error(f'failure authorize {login}: possibly may not exist')
         return render_template("auth.html", error=True)
     return render_template("auth.html")
 
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=5000, ssl_context="adhoc")
+    logging.info(f'server started at {datetime.now()}')
+    app.run(host="localhost", port=5000, ssl_context="adhoc", debug=True)
