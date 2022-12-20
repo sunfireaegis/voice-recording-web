fb924c3c48cd39056727089e0b8428d479649009
akiko23 <usksoaoko@gmail.com>
2022-12-20 15:00:11 +0300

    i am gigachad

--- app.py
+++ app.py
@@ -6,12 +6,11 @@ import os
 import csv
 import logging
 
-
 if not os.path.exists('log.txt'):
     os.system('touch log.txt')
 
 flaskLogger = logging.getLogger('werkzeug')
-flaskLogger.setLevel(logging.CRITICAL) # suppressing some unnecesary logs
+flaskLogger.setLevel(logging.CRITICAL)  # suppressing some unnecesary logs
 logging.basicConfig(filename='log', level=logging.DEBUG)
 
 connection = sqlite3.connect("database.db", check_same_thread=False)
@@ -31,13 +30,18 @@ file_list = os.listdir('csv/')
 
 to_read = dict()
 num_list = set()
+
+
 def read_file(name):
     with open(f"csv/{name}", "r", encoding="utf-8") as task:
         reader = csv.reader(task, delimiter=',')
         head = reader.__next__()
         for i, elem in enumerate(reader):
             num_list.add(f'{elem[2]}\n')
-            to_read.update({elem[2]: [elem[:2], False, None, set()]})  # tasl_id: data{text, task}, already_read, taken, skipped_by{uname}
+            to_read.update({elem[2]: [elem[:2], False, None,
+                                      set()]})  # tasl_id: data{text, task}, already_read, taken, skipped_by{uname}
+
+
 read_file('task.csv')
 used = set()
 used.add('task.csv')
@@ -50,18 +54,17 @@ def page_after_auth():
     uname = request.args['uname']
     for el in to_read:
         cur = to_read[el]
-        if not cur[1] and not (uname in cur[3]) and cur[2] is None: # if text is free yet
+        if not cur[1] and not (uname in cur[3]) and cur[2] is None:  # if text is free yet
             to_read[el][2] = uname
             return render_template("index.html", title="testt", text=cur[0][0], task=cur[0][1], n=el, uname=uname)
-        elif cur[2] == uname: # if you reloaded page (state of 'taken' arg is the same)
+        elif cur[2] == uname:  # if you reloaded page (state of 'taken' arg is the same)
             return render_template("index.html", title="testt", text=cur[0][0], task=cur[0][1], n=el, uname=uname)
 
     fnames = set(os.listdir('csv'))
-    for i in fnames-used:
+    for i in fnames - used:
         read_file(i)
         used.add(i)
         return redirect(url_for("page_after_auth", uname=uname), code=307)
-
 
     return render_template('index.html', title='task', text="Заданий больше нет", task=None, n=-1, uname=uname)
 
@@ -80,21 +83,19 @@ def get_file():
         is_skipped = request.form.get('skip')
 
         try:
-            to_read[cur_task][2] = False # current text broke free
+            to_read[cur_task][2] = False  # current text broke free
         except KeyError:
             app.logger.warning('Unable to save EOF')
 
-
-
-        if is_skipped =='true': # handling text skipping
+        if is_skipped == 'true':  # handling text skipping
             logging.warning(f'Task {cur_task} Skipped')
             try:
                 to_read[cur_task][3].add(author)
             except KeyError:
                 app.logger.warning('Unable to save EOF')
-        elif cur_task != '-1': # else saving file
+        elif cur_task != '-1':  # else saving file
             try:
-                to_read[cur_task][1] = True # setting that handled text is already read
+                to_read[cur_task][1] = True  # setting that handled text is already read
             except KeyError:
                 app.logger.warning('Unable to save EOF')
 
@@ -119,10 +120,13 @@ def reg():
         password = request.form.get("psw")
 
         with connection:
-            successful_auth = cursor.execute("INSERT INTO users (login, password) VALUES (?, ?)", (login, password,))
-            print(successful_auth)
-        logging.info(f'user {login} registrated succesfully')
-        return redirect(url_for("page_after_auth", uname=login), code=307)
+            possible_user = cursor.execute("SELECT login FROM users WHERE login=?", (login,)).fetchall()
+            if not bool(len(possible_user)):
+                cursor.execute("INSERT INTO users (login, password) VALUES (?, ?)", (login, password,))
+                logging.info(f'user {login} registrated succesfully')
+
+                return redirect(url_for("page_after_auth", uname=login), code=307)
+            return render_template("reg.html", userExists=True)
     return render_template("reg.html")
 
 
@@ -134,7 +138,8 @@ def auth():
         password = request.form.get("psw")
 
         with connection:
-            successful_auth = bool(len(cursor.execute("SELECT * FROM users WHERE login=? and password=?", (login, password,)).fetchall()))
+            successful_auth = bool(
+                len(cursor.execute("SELECT * FROM users WHERE login=? and password=?", (login, password,)).fetchall()))
             app.logger.warning(successful_auth)
         if successful_auth:
             # return redirect(url_for("page_after_auth"), )\
